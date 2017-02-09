101e1b23bb1dba8e63d000fc584ee37553def2c1
jack <jackmahonyy@gmail.com>
2017-02-09 21:20:05 +1000

    Revert "Added personal website link to footer - Added Wow.js to homepage elements - fixed column Aligment on hompage"
    This reverts commit da46558579d57c79626bc6649c4e87695ffc579e.

--- gulpfile.js
+++ gulpfile.js
@@ -57,7 +57,7 @@ gulp.task('watch', function() {
 	gulp.watch(paths.compiled).on('change', browserSync.reload);
 
   browserSync.init({
-      proxy: "http://localhost/jackmahony/"
+      proxy: "http://localhost:8888/jackmahony/"
   });
 });
 
